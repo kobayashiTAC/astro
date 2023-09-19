a84dd88d855ba4d5ee2174efc0fa96157f56da03
kobayashiTAC <kobayashi@trust-medical.jp>
2023-09-19 09:30:35 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    // TODO::Astroのtransitionを利用する場合
+    // experimental: {
+    //     viewTransitions: true
+    // }
+});
